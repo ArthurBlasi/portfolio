fbb3fe67e6825f5f5261f88c025224715a43b25e
ArthurBlasi <rthrblasi@gmail.com>
2024-10-17 16:21:14 -0300

    tirando foto errada

--- src/pages/Home/sections/Hero/components/frame/frameComponent.tsx
+++ src/pages/Home/sections/Hero/components/frame/frameComponent.tsx
@@ -5,7 +5,7 @@ export function FrameComponent() {
     return (
         <div className="frame-externo">
             <div className="foto">
-                <img src="C:\Users\arthu\Documents\PUCRS\GrupoDeEstudo\portfolio\src\assets\foto-formatura.jpg" alt="por favor vai" />
+                
             </div>
         </div>
     );
