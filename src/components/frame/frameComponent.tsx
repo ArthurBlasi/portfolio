502b092451e388184f4b21faba162f61955b3f01
ArthurBlasi <rthrblasi@gmail.com>
2024-10-17 11:09:54 -0300

    iniciando desenvolvimento

---
+++ src/components/frame/frameComponent.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './frameComponent.css';
+
+
+
+export function FrameComponent() {
+
+    return (
+        <div className="frame-externo">
+            <div className="foto">
+                
+            </div>
+        </div>
+    );
+}
