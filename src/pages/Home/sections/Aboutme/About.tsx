6f7fb86f34e4473202a032ab7e6a7ef1a41463d4
ArthurBlasi <rthrblasi@gmail.com>
2024-10-18 01:30:55 -0300

    salvando progresso

--- src/pages/Home/sections/Aboutme/About.tsx
+++ src/pages/Home/sections/Aboutme/About.tsx
@@ -1,9 +1,26 @@
 import './About.css';
+import fotolivro from '/e-book.png';
+import fotocerebro from '/cerebro.png';
+import fotocorrida from '/corrida.png';
+
+//import fotolivro from 'C:\Users\arthu\Documents\PUCRS\GrupoDeEstudo\portfolio\src\assets\e-book.png';
+//import fotocerebro from 'C:\Users\arthu\Documents\PUCRS\GrupoDeEstudo\portfolio\src\assets\cerebro.png';
+//import fotocorrida from 'C:\Users\arthu\Documents\PUCRS\GrupoDeEstudo\portfolio\src\assets\corrida.png';
+//<BlocoComponent icone={fotolivro} name={"My life journey"} text={"aaa"}/>
+//<BlocoComponent icone={fotocerebro} name={"What motivates me"} text={"aaa"}/>
+//<BlocoComponent icone={fotocorrida} name={"Beyond the code"} text={"aaa"}/>
 
 const About = () => {
   return (
     <section id="about-me" className="section about-me-section">
-      <h1>About Me</h1>
+      <div className='titulo-about'>
+        <h1>About Me</h1>
+      </div>
+      <div className='blocos'>
+      <BlocoComponent icone={fotolivro} name={"My life journey"} text={"aaa"}/>
+      <BlocoComponent icone={fotocerebro} name={"What motivates me"} text={"aaa"}/>
+      <BlocoComponent icone={fotocorrida} name={"Beyond the code"} text={"aaa"}/>
+      </div>
     </section>
   );
 };
---
+++ src/pages/Home/sections/Aboutme/blocos/blocoComponent.tsx
@@ -0,0 +1,20 @@
+import "./blocoComponent.css";
+
+export function BlocoComponent({ icone, name, text }: { icone: string, name: string, text: string }) {
+    //export function BlocoComponent({ name, text }: { name: string, text: string }) {
+
+    return (
+        <div className="bloco-geral">
+            <div className="icon">
+                <img src={icone} alt="" />
+            </div>
+            <div className="nome">
+                <h2>{name}</h2>
+            </div>
+            <div className="texto">
+                {text}
+            </div>
+        </div>
+        
+    );
+}
