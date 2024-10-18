a995e0038fc40d22020831008eae236890e68edd
ArthurBlasi <rthrblasi@gmail.com>
2024-10-18 03:59:36 -0300

    colocando imagens

--- src/pages/Home/sections/Techsnskills/Techsnskills.tsx
+++ src/pages/Home/sections/Techsnskills/Techsnskills.tsx
@@ -9,21 +9,18 @@ const Techsnskills = () => {
       </div>
       <div className='content'>
         <div className='bloco-grid'>
-          <TnsComponent/>
-          <TnsComponent/>
-          <TnsComponent/>
-          <TnsComponent/>
-          <TnsComponent/>
-          <TnsComponent/>
-          <TnsComponent/>
-          <TnsComponent/>
-          <TnsComponent/>
-          <TnsComponent/>
-          <TnsComponent/>
-          <TnsComponent/>
-          <TnsComponent/>
-          <TnsComponent/>
-          <TnsComponent/>
+          <TnsComponent icone='public\assets\java.png'/>
+          <TnsComponent icone='public\assets\python.png'/>
+          <TnsComponent icone='public\assets\react.png'/>
+          <TnsComponent icone='public\assets\github.png'/>
+          <TnsComponent icone='public\assets\typescript.png'/>
+          <TnsComponent icone='public\assets\oracle.png'/>
+          <TnsComponent icone='public\assets\sql-server.png'/>
+          <TnsComponent icone='public\assets\java-script.png'/>
+          <TnsComponent icone='public\assets\design.png'/>
+          <TnsComponent icone='public\assets\mongodb.png'/>
+          <TnsComponent icone='public\assets\teamwork.png'/>
+          <TnsComponent icone='public\assets\figma.png'/>
         </div>
         <div className='grafico'>
 
